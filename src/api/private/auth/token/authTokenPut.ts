80c2b5e6f876e3cc45a7bc033e073cb03e2bb956
Denis Zhmurenko <d.zhmurenko@dodobrands.io>
2025-01-11 15:56:52 +0000

    fix build error

--- src/api/private/auth/token/authTokenPut.ts
+++ src/api/private/auth/token/authTokenPut.ts
@@ -1,10 +1,10 @@
-import {Route} from "../../../types/routes";
-import {withErrorHandler} from "../../../middlewares/withErrorHandler";
-import {withAuth} from "../../../middlewares/withAuth";
-import {db} from "../../../db";
-import {randomString} from "../../../utils/randomString";
-import {ok} from "../../../utils/responses";
-import {Token} from "../../../types/domain/private";
+import {Route} from "../../../../types/routes";
+import {withErrorHandler} from "../../../../middlewares/withErrorHandler";
+import {withAuth} from "../../../../middlewares/withAuth";
+import {db} from "../../../../db";
+import {randomString} from "../../../../utils/randomString";
+import {ok} from "../../../../utils/responses";
+import {Token} from "../../../../types/domain/private";
 
 export const authTokenPut: Route = (f) =>
     f.put('/token', withErrorHandler(withAuth(async (_, resp, user) =>{
