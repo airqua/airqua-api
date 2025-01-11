3930fdd16ecd7e2eb27932ff5518afbb529f74b6
Denis Zhmurenko <d.zhmurenko@dodobrands.io>
2025-01-11 22:53:07 +0000

    unverify user if email is changed

--- src/api/private/account/accountPatch.ts
+++ src/api/private/account/accountPatch.ts
@@ -11,7 +11,7 @@ type RouteType = { Body: AccountPatch };
 
 export const accountPatch: Route = (f) =>
     f.patch<RouteType>('/', withErrorHandler(withAuth(async (req, resp, user) => {
-        const { old_password, ...rest } = req.body;
+        const { old_password, first_name, last_name, email, password } = req.body;
 
         if(!await bcrypt.compare(user.password, old_password)) {
             throw new BadRequestError();
@@ -20,12 +20,15 @@ export const accountPatch: Route = (f) =>
         await db.users.update({
             where: { id: user.id },
             data: {
-                ...rest,
-                password: rest.password ? await bcrypt.hash(rest.password, 12) : undefined
+                first_name: first_name || undefined,
+                last_name: last_name || undefined,
+                email: email || undefined,
+                password: password ? await bcrypt.hash(password, 12) : undefined,
+                verified: email ? false : undefined
             }
         });
 
-        if(rest.password) {
+        if(password) {
             await db.sessions.deleteMany({
                 where: { user_id: user.id }
             });
